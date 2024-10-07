aec5a3e1c051f179e6c8750ff0dde73acd4b53ef
Andy Christopher <ac5672021@gmail.com>
2024-10-06 19:40:47 -0500

    chapter one data types exercises

---
+++ Data_Types/tuple.py
@@ -0,0 +1,31 @@
+# Value	Type	Class
+# 'True'	String	str
+# False	Boolean	bool
+# (1, 2, 3)	Tuple	tuple
+# 1.5	Float	float
+# [1, 2, 3]	List	list
+# 2	Integer	int
+# range(5)	Range	range
+# {1, 2, 3}	Set	set
+# None	NoneType	NoneType
+# {'foo': 'bar'}	Dictionary	dict
+
+names = ('Asta', 'Butterscotch', 'Pudding', 'Neptune', 'Darwin')
+# print(names)
+
+# Name	Animal
+# Asta	dog
+# Butterscotch	cat
+# Pudding	cat
+# Neptune	fish
+# Darwin	lizard
+
+pets = {
+    'Asta': 'dog', 
+    'Butterscotch': 'cat',
+    'Pudding': 'cat',
+    'Neptune': 'fish',
+    'Darwin': 'lizard',
+}
+
+print(pets['Pudding'])
