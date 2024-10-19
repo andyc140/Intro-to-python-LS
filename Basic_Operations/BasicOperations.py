15bebb5ddd3a314eeb5af250e00077589375ac68
Andy Christopher <ac5672021@gmail.com>
2024-10-19 14:22:39 -0500

    chapter 2 operations exercises

---
+++ Basic_Operations/BasicOperations.py
@@ -0,0 +1,35 @@
+# Concatenate two strings, one with your first name and one with your last, to create a new string with your full name as its value
+
+# print('Andy' + ' Christopher')
+
+# Use the REPL and the arithmetic operators to extract the individual digits of 4936:
+
+# One place is 6.
+# Tens place is 3.
+# Hundreds place is 9.
+# Thousands place is 4.
+
+#num = 4936
+#print(num % 10)
+
+#print(num % 100 // 10)
+
+#print(num % 1000 // 100)
+
+#print(num // 1000)
+
+#Refactor the code from the previous exercise to use coercion to print 15 instead
+
+#print(5 + 10)
+
+#Will an error occur if you try to access a list element with an index greater than or equal to the list's length?
+#Yes, an error will occur: list index out of range. When you use an index value with no corresponding element, Python raises an IndexError error.
+
+#What will the following code do? Why?
+#print(int('3.1415'))
+#It raises a ValueError since the string value 3.1415 does not represent a valid integer.
+
+#To what value does the following expression evaluate?
+#print('12' < '9')
+#The expression evaluates as True since the operands are strings, not numbers. Python performs a character-by-character comparison from left to right when comparing string values. Thus, on the first comparison, it determines that '1' < '9', so '12' must be less than '9'.
+#If you used numbers instead of strings, the expression would be False.
